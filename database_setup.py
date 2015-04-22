9d341b79fc50df572d2f692b04f8541281ba4bfb
Brian Schuster <brian.daniel.schuster@gmail.com>
2015-04-22 06:08:16 -0400

    Reloaded restaruants db

--- database_setup.py
+++ database_setup.py
@@ -10,6 +10,7 @@ class Restaurant(Base):
 	__tablename__= 'restaurant'
 	name = Column(String(80), nullable = False)
 	id = Column(Integer, primary_key = True)
+	description = Column(String(1000))
 
 class MenuItem(Base):
 	__tablename__= 'menu_item'
